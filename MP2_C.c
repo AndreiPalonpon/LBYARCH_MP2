a40c2b79eee3b63bfd8986e43e89a3519ecb624b
AndreiPalonpon <andrei_palonpon@dlsu.edu.ph>
2024-11-24 15:55:26 +0800

    Implement feature to allocate and input arrays

--- MP2_C.c
+++ MP2_C.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 extern void asmfunc(int n, double *arr1, int *arr2);
 
 int main()
 {
     // Sample
-    double vec1[3][3] = {{0.0, 62.5, 10.1},
-                         {60.0, 122.3, 5.5},
-                         {30.0, 160.7, 7.8}};
-    int vec2[3] = {0, 0, 0};
+    int n;
+    scanf("%d", &n);
 
-    asmfunc(3, vec1[0], vec2);
+    double *vec1 = (double *)calloc(n * 3, sizeof(double));
+    int *vec2 = (int *)calloc(n, sizeof(int));
 
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%lf %lf %lf", (vec1 + 3 * i), (vec1 + 3 * i + 1), (vec1 + 3 * i + 2));
+    }
+
+    asmfunc(n, vec1, vec2);
+
+    for (int i = 0; i < n; i++)
         printf("%d\n", vec2[i]);
+
+    free(vec1);
+    free(vec2);
 }
