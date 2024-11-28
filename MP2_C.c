acccd3f1fed3c1aa91cf819942bcdd4ca70e6131
Daniel Gavrie Clemente <69893252+11daniel@users.noreply.github.com>
2024-11-28 11:51:50 +0800

    Update MP2_C.c

--- MP2_C.c
+++ MP2_C.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 extern void asmfunc(int n, double *arr1, int *arr2);
 
@@ -17,11 +18,18 @@ int main()
         scanf("%lf %lf %lf", (vec1 + 3 * i), (vec1 + 3 * i + 1), (vec1 + 3 * i + 2));
     }
 
+    clock_t start = clock();
+
     asmfunc(n, vec1, vec2);
+
+    clock_t end = clock();
 
     for (int i = 0; i < n; i++)
         printf("%d\n", vec2[i]);
 
     free(vec1);
     free(vec2);
+
+    double exec_time = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Total Execution Time: %d seconds\n", exec_time);
 }
